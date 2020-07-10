6d4bcab597ded505ae559d48cb02aaa5b744cdc6
Arthur Lorencini Bergamaschi <arthur.bergamaschi@aluno.ufes.br>
2020-07-10 17:43:54 -0300

    02

---
+++ 02-hello_again/src/02-hello_again.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "/home/arthur/GitHub/particle-photon-4fun/02-hello_again/src/02-hello_again.ino"
+/*
+ * Project 02-hello_again
+ * Description:
+ * Author: Arthur Lorencini Bergamaschi
+ * Date: 13/04/2020 (dmy)
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/home/arthur/GitHub/particle-photon-4fun/02-hello_again/src/02-hello_again.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
---
+++ 02-hello_again/target/1.5.0/photon/obj/src/user_module.o.d
@@ -0,0 +1,4 @@
+/home/arthur/GitHub/particle-photon-4fun/02-hello_again/target/1.5.0/photon//obj/src/user_module.o: \
+ src/user_module.c ../../../dynalib/inc/dynalib.h
+
+../../../dynalib/inc/dynalib.h:
