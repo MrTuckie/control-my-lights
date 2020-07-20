3746b00b0de6fafb06a68a30fa5900d64e3e570d
Glitch (control-light) <none>
2020-07-20 18:09:23 +0000

    🌺📰 Updated with Glitch - adding web project

---
+++ app.py
@@ -0,0 +1,67 @@
+import os
+
+from dotenv import load_dotenv
+from flask import (
+    Flask,
+    flash, 
+    render_template, 
+    redirect,
+    request,
+    url_for,
+)
+
+import pyparticle as pp
+
+load_dotenv()
+app = Flask(__name__)
+app.secret_key = "ssssh don't tell anyone"
+
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
+DEVICE_ID = os.getenv('DEVICE_ID')
+
+
+particle = pp.Particle(access_token=AUTH_TOKEN)
+devices = particle.list_devices()
+device = devices[0]
+
+def get_sent_messages():
+    # TODO: Make this return a collection of messages that were sent from the number
+    messages = []
+    return messages
+
+def send_message(to, body):
+    # TODO: Send the text message
+    pass
+
+@app.route("/", methods=["GET"])
+def index():
+    try:
+      messages = get_sent_messages()
+      status = particle.get_variable(device['id'], 'test')
+      if status['result']:
+        flash(f"A lâmpada está ligada, meu cachorrão")
+      else:
+          flash(f"A lâmpada está desligada, meu cachorrinho")
+      return render_template("index.html", messages=messages)
+    
+    except:
+      flash(f"Erro. Provavelmente a lâmpada está desconectada.")
+      return render_template("index.html", messages=messages)
+
+@app.route("/on", methods=["POST"])
+def on():
+    particle.call_function(device['id'], 'pisca', 1)
+    flash('PISCOU :)')
+    return redirect(url_for('index'))
+
+  
+@app.route("/off", methods=["POST"])
+def off():
+    particle.call_function(device['id'], 'pisca', 0)
+    flash('DESLIGOU :(')
+    return redirect(url_for('index'))
+  
+
+
+if __name__ == '__main__':
+    app.run()
